f63f2ca555af494ad6dbdf9bfa5606678a2bb590
gitee_com_zb <18749379836@163.com>
2023-07-14 21:54:49 +0800

    string模拟实现

--- test_7_13/string.h
+++ test_7_13/string.h
@@ -7,33 +7,30 @@ namespace lzb
 	class string
 	{
 	public:
-		typedef char* iterator;
-		typedef const char* const_iterator;
-
-		iterator begin()
-		{
-			return _str;
-		}
-		iterator end()
-		{
-			return _str + _size;
-		}
-		const_iterator begin() const
-		{
-			return _str;
-		}
-		const_iterator end() const
-		{
-			return _str + _size;
-		}
-
+		//默认构造--“初始化”
 		string(const char* str = "") //注意这里的空字符串,其实有‘\0’
-		{								 //为了解决未初始化对象的默认构造函数的问题
+		{							//为了解决"未初始化对象"的默认构造函数的问题
 			_size = strlen(str);
 			_capacity = _size;
 			_str = new char[_capacity+1];
 			strcpy(_str, str);
 		}
+		//清理
+		~string()
+		{
+			delete[] _str;
+			_str = nullptr;
+			_size = _capacity = 0;
+		}
+		//拷贝构造
+		string(const string& s)
+		{
+			_str = new char[s._capacity + 1];
+			strcpy(_str, s._str);
+			_size = s._size;
+			_capacity = s._capacity;
+		}
+		//基础功能:返回内容,有效元素个数,支持下标访问
 		const char* c_str() const
 		{
 			return _str;
@@ -52,18 +49,169 @@ namespace lzb
 			assert(pos < _size);
 			return _str[pos];
 		}
-		~string()
+		//迭代器
+		typedef char* iterator;
+		typedef const char* const_iterator;
+		iterator begin()
 		{
-			delete[] _str;
-			_str = nullptr;
-			_size = _capacity = 0;
+			return _str;
+		}
+		iterator end()
+		{
+			return _str + _size;
+		}
+		const_iterator begin() const
+		{
+			return _str;
+		}
+		const_iterator end() const
+		{
+			return _str + _size;
+		}
+		//增删查改
+		void reserve(size_t n)
+		{
+			if (n > _capacity)
+			{
+				char* tmp = new char[n + 1];
+				strcpy(tmp, _str);
+				delete[] _str;
+				_str = tmp;
+				_capacity = n;
+			}
+		}
+		void push_back(char ch)
+		{
+			if (_size == _capacity)
+			{
+				reserve(_capacity == 0 ? 4 : _capacity * 2);
+			}
+			_str[_size++] = ch;
+			_str[_size] = '\0';
+		}
+		void append(const char* str)
+		{
+			size_t len = strlen(str);
+			if (_size + len > _capacity)
+			{
+				reserve(_size + len);
+			}
+			strcpy(_str + _size, str);
+			_size += len;
+		}
+		string& operator+=(char ch)
+		{
+			push_back(ch);
+			return *this;
+		}
+		string& operator+=(const char* str)
+		{
+			append(str);
+			return *this;
+		}
+		void insert(size_t pos, size_t n, char ch)
+		{
+			assert(pos <= _size);
+			if (_size + n > _capacity)
+			{
+				reserve(_size + n);
+			}
+			int end = (int)_size;
+			//解决pos为size_t与end比较整型提升的问题,end结束时为-1
+			//while (end >= (int)pos)
+			while(end >= pos && end != npos)
+			{
+				_str[end + n] = _str[end];
+				--end;
+			}
+			for (size_t i = 0; i < n; ++i)
+			{
+				_str[pos + i] = ch;
+			}
+			_size += n;
+		}
+		void insert(size_t pos, const char* str)
+		{
+			assert(pos <= _size);
+			size_t len = strlen(str);
+			if (_size + len > _capacity)
+			{
+				reserve(_size + len);
+			}
+			int end = (int)_size;
+			while (end >= pos && end != npos)
+			{
+				_str[end + len] = _str[end];
+				--end;
+			}
+			for (size_t i = 0; i < len; ++i)
+			{
+				_str[pos + i] = str[i];
+			}
+			_size += len;
+		}
+		void erase(size_t pos, size_t len = npos)
+		{
+			assert(pos <= _size);
+			if (len == npos || pos + len > _size) //意为pos后面内容全部删除
+			{
+				//_str[pos] = '\0';
+				_size = pos;
+				_str[_size] = '\0';
+			}
+			else
+			{
+				size_t end = pos + len;
+				while (end <= _size) //等号连同'\0'也可以移过来
+				{
+					_str[pos++] = _str[end++];
+				}
+				_size -= len;
+			}
+		}
+		size_t find(const char* str, size_t pos = 0)
+		{
+			assert(pos < _size);
+			const char* ptr = strstr(_str, str);
+			if (ptr)
+				return ptr - _str;
+			else
+				return npos;
+		}
+		size_t find(char ch, size_t pos = 0)
+		{
+			assert(pos < _size);
+			for (size_t i = pos; i < _size; ++i)
+			{
+				if (_str[i] == ch)
+					return i;
+			}
+			return npos;
+		}
+		string substr(size_t pos = 0, size_t len = npos)
+		{
+			assert(pos < _size);
+			size_t n = len;
+			if (len == npos || pos + len > _size)
+			{
+				n = _size - pos;
+			}
+			string tmp;
+			for (size_t i = pos; i < pos + n; ++i)
+			{
+				tmp += _str[i];
+			}
+			return tmp;
 		}
 	private:
 		char* _str;
 		size_t _size;
 		size_t _capacity;
+	public:
+		//static const size_t npos = -1;
+		static const size_t npos;
 	};
-
+	const size_t string::npos = -1;
 }
 
 
--- test_7_13/test.cpp
+++ test_7_13/test.cpp
@@ -13,10 +13,6 @@ void TestString1()
 	//std::cout << s2.c_str() << std::endl;
 	//std::string s2;
 	//std::cout << s2.c_str() << std::endl;
-	
-	/*lzb::string s3="";
-	std::cout << s3.c_str() << std::endl;
-	std::cout << s3.size() << std::endl;*/
 
 	for (size_t i = 0; i < s1.size(); ++i)
 	{
@@ -30,6 +26,7 @@ void TestString2()
 	lzb::string::iterator it = s1.begin();
 	while (it != s1.end())
 	{
+		//*it += 1;//非const对象可读可写
 		std::cout << *it << " ";
 		++it;
 	}
@@ -40,11 +37,115 @@ void TestString2()
 		std::cout << e ;
 	}
 	std::cout << std::endl;
+
+	const lzb::string s2("testconststring");
+	lzb::string::const_iterator cit = s2.begin();
+	//auto cit = s2.begin();
+	for (cit; cit != s2.end(); ++cit)
+	{
+		//*cit += 1; const对象只读
+		std::cout << *cit ;
+	}
+	std::cout << std::endl;
+}
+void TestString3()
+{
+	lzb::string s1("TestString3");
+	s1.push_back('#');
+	std::cout << s1.c_str() << std::endl;
+	s1.append("@you can see me");
+	std::cout << s1.c_str() << std::endl;
+	std::cout << "=========================================" << std::endl;
+
+	lzb::string s2("");
+	s2.push_back('$');
+	std::cout << s2.c_str() << std::endl;
+	s2.append("Test@you can see me");
+	std::cout << s2.c_str() << std::endl;
+	std::cout << "=========================================" << std::endl;
+
+	lzb::string s3("TestString");
+	s3 += '#';
+	s3 += '@';
+	std::cout << s3.c_str() << std::endl;
+	s3 += "you can see me!!!";
+	std::cout << s3.c_str() << std::endl;
 }
 
+void TestString4()
+{
+	lzb::string s1("TestStringInsert");
+	s1.insert(4, 3, '#');
+	std::cout << s1.c_str() << std::endl;
+	s1.insert(0, 5, '$');
+	std::cout << s1.c_str() << std::endl;
+	std::cout << "====================================" << std::endl;
+	lzb::string s2("TestStringInsertChar*");
+	s2.insert(4, "you can see me!!!");
+	std::cout << s2.c_str() << std::endl;
+	s2.insert(0, "@@@@@@@@");
+	std::cout << s2.c_str() << std::endl;
+}
+
+void TestString5()
+{
+	lzb::string s1("*****TestStringErase");
+	std::cout << s1.c_str() << std::endl;
+	s1.erase(0, 5);
+	std::cout << s1.c_str() << std::endl;
+	std::cout << "=======================================" << std::endl;
+	lzb::string s2("*****TestStringErase");
+	std::cout << s2.c_str() << std::endl;
+	s2.erase(5);
+	std::cout << s2.c_str() << std::endl;
+
+}
+void TestString6()
+{
+	//lzb::string s1("TestfindStringFind");
+	//std::cout << s1.find('S', 0) << std::endl;
+	//std::cout << s1.find('T', 0) << std::endl;
+	//std::cout << s1.find('T', 1) << std::endl;
+	//std::cout << "=========================================" << std::endl;
+	//std::cout << s1.find("Findis", 1) << std::endl;
+	//std::cout << s1.find("String", 1) << std::endl;
+	//std::cout << s1.find("find", 1) << std::endl;
+	// 
+	// 
+	//substr切割字符串
+	//配合find完成切割的功能 
+	// 
+	//string s1 = "https://legacy.cplusplus.com/reference/string/string/substr/";
+	//string s2;
+	//s2=s1.substr(0,s1.find("://"));
+	//cout << s2 << endl;
+	// 
+	using namespace std;
+	lzb::string url = "https://legacy.cplusplus.com/reference/string/string/substr/";
+	// 协议 域名 资源名
+	size_t pos1 = url.find("://");
+	if (pos1 != lzb::string::npos)
+	{
+		lzb::string agreement = url.substr(0, pos1);
+		cout << agreement.c_str() << endl;
+	}
+	size_t pos2 = url.find('/', pos1 + 3);
+	if (pos2 != lzb::string::npos)
+	{
+		lzb::string domain = url.substr(pos1 + 3, pos2 - (pos1 + 3));
+		cout << domain.c_str() << endl;
+	}
+	lzb::string uri = url.substr(pos2 + 1);
+	cout << uri.c_str() << endl;
+
+}
 int main()
 {
-	TestString1();
+	//TestString1();
 	//TestString2();
+	//TestString3();
+	//TestString4();
+	//TestString5();
+	TestString6();
 	return 0;
 }
